2ce68a0e7a9606e014a45e50d63aed0c313dcf90
LoganFZ <116685046+LoganFZ@users.noreply.github.com>
2022-11-01 13:49:13 -0600

    Primer avance

---
+++ src/index.js
@@ -0,0 +1,28 @@
+const http = require('http');
+const path = require('path');
+
+const express = require ('express');
+const socketio = require ('socket.io');
+
+const mongoose = require ("mongoose");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+// db connection
+mongoose.connect('mongodb://127.0.0.1/chat-database')
+  .then(db => console.log('db is connected'))
+  .catch(err => console.log(err));
+
+app.set('port', process.env.PORT || 3000);
+
+require('./sockets')(io);
+
+// static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// starting the server
+server.listen(app.get('port'), () => {
+  console.log('server on port', app.get('port'));
+});
